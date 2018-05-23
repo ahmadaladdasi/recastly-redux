1874911bccbbd61e1493b417230f782a10a65bb2
ahmadaladdasi <ahmadaladdasi88@gmail.com>
2018-05-23 13:11:44 -0700

    finished the store and containers

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleSearchInputChange: (query)=> dispatch(handleSearchChange(query))
+  };
+};
+
+var SearchContainer = () => {
+  return connect(null, mapDispatchToProps)(Search);
+};
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
 
-export default SearchContainer;
+export default SearchContainer();
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './../reducers/main.js';
+import combineReducer from './../reducers/main.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+export default createStore(
+  combineReducer,
+  applyMiddleware(thunk)
+);
